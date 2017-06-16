ffa0e6ed82a22975ccc1c7b8f25387edb3bdf369
mssngr <gabriel@ecliptic.io>
2017-06-16 10:47:10 -0600

    happy horse faces.

--- happiness.js
+++ happiness.js
@@ -2,26 +2,26 @@ module.exports = [
 	{
     breedName: 'This is Shiro',
     breedDesc: 'He\'s happy about everything',
-    breedImg: 'public/images/happy-1.png'
+    breedImg: 'public/images/happy-1.jpg'
   },
   {
     breedName: 'This is Felix',
     breedDesc: 'He\'s happy he has warm laundry to lay in',
-    breedImg: 'public/images/happy-2.png'
+    breedImg: 'public/images/happy-2.jpg'
   },
   {
     breedName: 'This is Smokey',
     breedDesc: 'She\'s happy and dreaming about chasing butterflies',
-    breedImg: 'public/images/happy-3.png'
+    breedImg: 'public/images/happy-3.jpg'
   },
   {
     breedName: 'This is Buford',
     breedDesc: 'He\'s happy you got out the laser pointer',
-    breedImg: 'public/images/happy-4.png'
+    breedImg: 'public/images/happy-4.jpg'
   },
   {
     breedName: 'This is Scuba',
     breedDesc: 'She\'s happy about head scratches',
-    breedImg: 'public/images/happy-5.png'
+    breedImg: 'public/images/happy-5.jpg'
   },
 ];
